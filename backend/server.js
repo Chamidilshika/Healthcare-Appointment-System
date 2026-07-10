5cf816645edf878adcf0d4047ebf366e24ddaf59
Chamidilshika <chamidilshika7@gmail.com>
2026-07-10 18:37:03 +0530

    modified server.js1

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const doctorRoutes = require("./routes/doctorRoutes");
 const appointmentRoutes = require("./routes/appointmentRoutes");
@@ -11,23 +12,43 @@ dotenv.config();
 
 const app = express();
 
-// middleware
-app.use(cors());
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/contact", contactRoutes);
+
 app.use("/uploads", express.static("uploads"));
 
-// test route
+// Test route
 app.get("/", (req, res) => {
   res.send("Backend API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+// Only run listen locally
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
-});
+  });
+}
+
+
+// Export for Vercel
+module.exports = app;
