24d23dec2c137590dda10e49edec4d0abc332084
Chamidilshika <chamidilshika7@gmail.com>
2026-07-10 17:01:34 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, ()=>{
+    console.log(`Server running on ${PORT}`);
 });
